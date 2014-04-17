914261854e45b9ecd0cf33080c37d710b50c3d42
Amirol Ahmad <9w2pdf@gmail.com>
2014-04-18 01:16:18 +0800

    nable Demographics and Interest Reports - GA

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -24,6 +24,7 @@
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
     ga('create', 'UA-50147916-1', 'antiscam.me');
+    ga('require', 'displayfeatures');
     ga('send', 'pageview');
 
   </script>
